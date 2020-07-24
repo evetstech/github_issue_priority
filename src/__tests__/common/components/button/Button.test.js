9d185a4bae84243873bbc2cdc6cd952a980ba9af
Steve Singrattana <steve@evetstech.com>
2020-07-23 23:04:36 -0400

    add a couple snapshots

--- src/__tests__/common/components/button/Button.test.js
+++ src/__tests__/common/components/button/Button.test.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import { mount } from 'enzyme';
 import Button from '../../../../common/components/button/Button';
+import renderer from 'react-test-renderer';
 
 describe('button', () => {
   it('should mount', () => {
     const wrapper = mount(<Button/>);
 
     expect(wrapper.length).toBeTruthy();
+  });
+
+  it('renders correctly', () => {
+    const tree = renderer
+      .create(<Button text={'hi'}/>)
+      .toJSON();
+    expect(tree).toMatchSnapshot();
   });
 });
--- src/__tests__/common/components/input/Input.test.js
+++ src/__tests__/common/components/input/Input.test.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import { mount } from 'enzyme';
 import Input from '../../../../common/components/input/Input';
+import renderer from 'react-test-renderer';
 
 describe('input', () => {
   it('should mount', () => {
     const wrapper = mount(<Input/>);
 
     expect(wrapper.length).toBeTruthy();
+  });
+
+  it('renders correctly', () => {
+    const tree = renderer
+      .create(<Input value={'hi'}/>)
+      .toJSON();
+    expect(tree).toMatchSnapshot();
   });
 });
